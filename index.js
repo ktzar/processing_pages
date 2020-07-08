cb488d8afaf93f1ca8f297f48b45853298ed5a3a
miguel <miguelmail@gmail.com>
2020-07-08 12:51:45 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const errorMessages = {
+    NO_STOCK: 'No stock has been found',
+    INCORRECT_DETAILS: 'Incorrect details have been entered' 
+};
+
+const generateErrorPage = message => { title: 'Error page', message };
+
+
+const stateProcessors = {
+    error: state => {
+        const { errorCode } = state;
+        if (errorCode === undefined || errorCode === null) {
+            return generateErrorPage(null);
+        }
+        if (errorMessages[errorCode]) {
+            return generateErrorPage(null);
+            return { title: 'Error page', message: errorMessages[errorCode] };
+        }
+        return { title: 'Error page', message: 'Unknown error code' };
+    },
+    processing: async (state) => {
+        await sleep(2000);
+        return false;
+    },
+    success: state => ({ title: 'Order complete', message: null })
+}
+
+const getProcessingPage = async function (states) {
+    for (let i = 0 ; i < states.length ; i ++) {
+        const {state} = states[i];
+
+        if (stateProcessors[state]) {
+            const result = await stateProcessors[state](states[i]);
+            if (result) {
+                return result;
+            }
+        } else {
+            return { title: 'Error page', message: 'Invalid state' };
+        }
+    }
+};
+
+// One of these cases should be uncommented to be run. One left uncommented so it runs.
+console.log('Processing and then undefined error');
+getProcessingPage([{state: 'processing'}, {state: 'error'}])
+    .then(console.log);
+
+/*
+console.log('Processing and then error with no stock');
+getProcessingPage([
+    {state: 'processing'},
+    {state: 'error', errorCode: 'NO_STOCK'}
+])
+    .then(console.log);
+*/
+
+/*
+console.log('Processing and then error with incorrect details');
+getProcessingPage([
+    {state: 'processing'},
+    {state: 'error', errorCode: 'INCORRECT_DETAILS'}
+])
+    .then(console.log);
+*/
+
+/*
+console.log('Invalid state');
+getProcessingPage([
+    {state: 'processing'},
+    {state: 'some_dodgy_state'}
+])
+    .then(console.log);
+*/
---
+++ readme.md
@@ -0,0 +1,6 @@
+ ## 8/7/2020
+
+ Please note that I've used asyncronous functions as a way to implement the delay requirement.
+ Another way would be to use callbacks, but as the signature of the function was provided and a callback wasn't specified, I decided to go down this way.
+ One of the use cases is left uncommented, but the others can be uncommented (one by one, unless multiple states should be processed at the same time)
+
